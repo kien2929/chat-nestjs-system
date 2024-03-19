469dd8e1dea5c30eaa1b4bd7d74f28df630c0b3b
Kien <kien2929@gmail.com>
2024-03-19 10:51:00 +0700

    :+1: Add share service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,29 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { SharedService } from '@app/shared';
+import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const configService = app.get(ConfigService);
+  const sharedService = app.get(SharedService);
+  const queue = configService.get('RABBITMQ_AUTH_QUEUE');
 
-  const USER = configService.get('RABBITMQ_USER');
-  const PASSWORD = configService.get('RABBITMQ_PASS');
-  const HOST = configService.get('RABBITMQ_HOST');
-  const QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-      queue: QUEUE,
-      noAck: false,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
+  app.connectMicroservice<MicroserviceOptions>(
+    sharedService.getRmqOptions(queue),
+  );
   app.startAllMicroservices();
 }
 bootstrap();
--- apps/presence/src/main.ts
+++ apps/presence/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { SharedService } from '@app/shared';
 import { PresenceModule } from './presence.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PresenceModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const sharedService = app.get(SharedService);
+  const queue = configService.get('RABBITMQ_PRESENCE_QUEUE');
+
+  app.connectMicroservice<MicroserviceOptions>(
+    sharedService.getRmqOptions(queue),
+  );
+  app.startAllMicroservices();
 }
 bootstrap();
