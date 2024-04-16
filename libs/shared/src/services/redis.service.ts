d6c46d951259cea8741076292ad8bc534cc3370c
Kien <kien2929@gmail.com>
2024-04-16 10:31:45 +0700

    :heavy_plus_sign: Add Redis cache manager

---
+++ libs/shared/src/services/redis.service.ts
@@ -0,0 +1,20 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+@Injectable()
+export class RedisService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  async get(key: string) {
+    return await this.cacheManager.get(key);
+  }
+
+  async set(key: string, value: any) {
+    return await this.cacheManager.set(key, value);
+  }
+
+  async del(key: string) {
+    return await this.cacheManager.del(key);
+  }
+}
